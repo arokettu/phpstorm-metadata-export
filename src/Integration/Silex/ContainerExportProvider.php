8f1e341a21a8a02434006382570d7dc7792cd447
Anton Smirnov <sandfox@sandfox.me>
2018-02-22 02:30:08 +0200

    Silex integration provider

---
+++ src/Integration/Silex/ContainerExportProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SandFoxMe\PhpStorm\Metadata\Integration\Silex;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use SandFoxMe\PhpStorm\Metadata\Generator;
+use Silex\Api\BootableProviderInterface;
+use Silex\Application;
+
+class ContainerExportProvider implements ServiceProviderInterface, BootableProviderInterface
+{
+    public function register(Container $pimple)
+    {
+        // try to find root path of the project (parent of vendor)
+        // remove path postfix vendor/sandfoxme/phpstorm-metadata-export/src/Integration/Silex
+        $pimple['phpstorm.metadata.filename'] = realpath(__DIR__ . '/../../../../../..') .
+            '/.phpstorm.meta.php/sandfoxme_container_export_silex.meta.php';
+
+        $pimple['phpstorm.metadata.options'] = [];
+
+        $pimple['phpstorm.metadata.only_debug'] = true;
+    }
+
+    public function boot(Application $app)
+    {
+        if (!$app['phpstorm.metadata.only_debug'] || $app['debug']) {
+            $app->finish(function () use ($app) {
+                try {
+                    Generator::store($app['phpstorm.metadata.filename'], [$app], $app['phpstorm.metadata.options']);
+                } catch (\Throwable $e) {
+                    // ignore
+                }
+            }, Application::LATE_EVENT);
+        }
+    }
+}
