7daffe5413ffc56041def0a855c85ef530a92fca
Anton Smirnov <sandfox@sandfox.me>
2021-09-29 23:14:28 +0300

    Fix test on lowest DI

--- tests/PhpDITest.php
+++ tests/PhpDITest.php
@@ -9,7 +9,6 @@ use PHPUnit\Framework\TestCase;
 use SandFox\PhpStorm\Metadata\Generator;
 
 use function DI\create;
-use function DI\value;
 
 class PhpDITest extends TestCase
 {
@@ -17,11 +16,13 @@ class PhpDITest extends TestCase
     {
         $di = new Container();
 
-        $di->set(\ArrayObject::class, create());
+        $di->set(\ArrayObject::class, create()->constructor([], 0, \ArrayIterator::class));
         $di->set('string', function () {
             return 'path';
         });
-        $di->set('int', value(123));
+        $di->set('int', function () {
+            return 123;
+        });
         $di->set('errored', function (): void {
             throw new \RuntimeException('Unable to init');
         });
